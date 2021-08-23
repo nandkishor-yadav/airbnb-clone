b1a8338901e0c279e67ff2878dfc9918e3569326
Nandkishor.Y <Nandkishor.Yadav@wrendata.com>
2021-08-23 17:01:04 +0100

    Initial commit

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p className="cursor-pointer">How Airbnb works</p>
+        <p className="cursor-pointer">Newsroom</p>
+        <p className="cursor-pointer">Investors</p>
+        <p className="cursor-pointer">Airbnb Plus</p>
+        <p className="cursor-pointer">Airbnb Luxe</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p className="cursor-pointer">How Airbnb works</p>
+        <p className="cursor-pointer">Newsroom</p>
+        <p className="cursor-pointer">Investors</p>
+        <p className="cursor-pointer">Airbnb Plus</p>
+        <p className="cursor-pointer">Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p className="cursor-pointer">How Airbnb works</p>
+        <p className="cursor-pointer">Newsroom</p>
+        <p className="cursor-pointer">Investors</p>
+        <p className="cursor-pointer">Airbnb Plus</p>
+        <p className="cursor-pointer">Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p className="cursor-pointer">How Airbnb works</p>
+        <p className="cursor-pointer">Newsroom</p>
+        <p className="cursor-pointer">Investors</p>
+        <p className="cursor-pointer">Airbnb Plus</p>
+        <p className="cursor-pointer">Airbnb Luxe</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
+
+// https://flexboxfroggy.com/
